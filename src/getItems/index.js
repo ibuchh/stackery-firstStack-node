59c3ff262db1772f7443e9db0565dd8a7240b862
Irshad A Buchh <ibuchh@gmail.com>
2020-07-27 00:10:10 -0500

    added function code

--- src/getItems/index.js
+++ src/getItems/index.js
@@ -1,6 +1,39 @@
-exports.handler = async (event, context) => {
-  // Log the event argument for debugging and for use in local development.
-  console.log(JSON.stringify(event, undefined, 2));
+// exports.handler = async (event, context) => {
+//   // Log the event argument for debugging and for use in local development.
+//   console.log(JSON.stringify(event, undefined, 2));
 
-  return {};
+//   return {};
+// };
+
+const AWS = require('aws-sdk');
+
+exports.handler = async (event, context) => {
+  // Use dynamodb to get items from the Items table
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+  const params = {
+    TableName: process.env.TABLE_NAME
+  };
+
+  let allItems = [];
+
+  try {
+    console.log(`Getting data from table ${process.env.TABLE_NAME}.`);
+    const items = await dynamodb.scan(params).promise(); // get items from DynamoDB
+    items.Items.forEach((item) => allItems.push(item)); // put contents in an array for easier parsing
+    allItems.forEach(item => console.log(`Item ${item.id}: ${item.content}\n`)); // log the contents
+  } catch (error) {
+    console.log(`Error getting data from table ${process.env.TABLE_NAME}. Make sure this function is running in the same environment as the table.`);
+    throw new Error(error); // stop execution if data from dynamodb not available
+  }
+
+  // Return a 200 response if no errors
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: `${allItems.length} items found `
+  };
+
+  return response;
 };
--- src/newItem/index.js
+++ src/newItem/index.js
@@ -1,6 +1,43 @@
-exports.handler = async (event, context) => {
-  // Log the event argument for debugging and for use in local development.
-  console.log(JSON.stringify(event, undefined, 2));
+// exports.handler = async (event, context) => {
+//   // Log the event argument for debugging and for use in local development.
+//   console.log(JSON.stringify(event, undefined, 2));
 
-  return {};
+//   return {};
+// };
+
+const AWS = require('aws-sdk');
+var shortid = require('shortid');
+
+exports.handler = async (event, context) => {
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+  const params = {
+    TableName: process.env.TABLE_NAME, // get the table name from the automatically populated environment variables
+    Item: {
+      id: shortid.generate, // modify with each invoke so the id does not repeat
+      content: 'This is my content' // modify content here
+    },
+    ConditionExpression: 'attribute_not_exists(id)', // do not overwrite existing entries
+    ReturnConsumedCapacity: 'TOTAL'
+  };
+
+  try {
+    // Write a new item to the Items table
+    await dynamodb.put(params).promise();
+    console.log(`Writing item ${params.Item.id} to table ${process.env.TABLE_NAME}.`);
+  } catch (error) {
+    console.log(`Error writing to table ${process.env.TABLE_NAME}. Make sure this function is running in the same environment as the table.`);
+    throw new Error(error); // stop execution if dynamodb is not available
+  }
+
+  // Return a 200 response if no errors
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: 'Success!'
+  };
+
+  return response;
 };
+
